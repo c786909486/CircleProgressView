190ea79f2c49632f6a8bb940aa8e6ab3575f5fdf
c786909486 <786909486@qq.com>
2018-03-05 13:57:38 +0800

    更新

--- app/src/main/java/com/ckz/circleprogressview/MainActivity.java
+++ app/src/main/java/com/ckz/circleprogressview/MainActivity.java
@@ -2,12 +2,15 @@ package com.ckz.circleprogressview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.ckz.circleprogress.CircleProgressView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CircleProgressView mShowProgress;
+    private TextView mShowText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         mShowProgress = (CircleProgressView) findViewById(R.id.show_progress);
         mShowProgress.setProgress(50f);
+        mShowText = (TextView) findViewById(R.id.show_text);
+        mShowText.setText("显示进度条");
+        mShowText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mShowProgress.setEnableAni(true);
+                mShowProgress.setProgress(95f);
+            }
+        });
     }
 }
--- circleprogress/src/main/java/com/ckz/circleprogress/CircleProgressView.java
+++ circleprogress/src/main/java/com/ckz/circleprogress/CircleProgressView.java
@@ -13,9 +13,6 @@ import android.view.animation.LinearInterpolator;
 
 import java.text.DecimalFormat;
 
-/**
- * Created by Administrator on 2018/3/2.
- */
 
 public class CircleProgressView extends View {
 
@@ -25,15 +22,16 @@ public class CircleProgressView extends View {
     private int circleSize = 15;//圆弧大小
     private int textSize = 64;//字体大小
     private int bgCircleColor = Color.parseColor("#bbbbbb");//圆弧背景色
-    private int circleColor = Color.WHITE;//进度圆弧颜色
+    private int progressColor = Color.WHITE;//进度圆弧颜色
     private int textColor = Color.WHITE;//字体颜色
     private int textWidth;
     private int textHeight;
     private boolean needPersent = true;//是否需要百分号
-
     private float progress = 0;//进度
     private float startAngle = -225f;//开始角度
     private float drawAngle = 270f;//绘制角度
+
+    private boolean needAni = false;
 
     public CircleProgressView(Context context) {
         this(context,null);
@@ -57,7 +55,7 @@ public class CircleProgressView extends View {
 
         progressPaint = new Paint();
         progressPaint.setAntiAlias(true);
-        progressPaint.setColor(circleColor);
+        progressPaint.setColor(progressColor);
         progressPaint.setStyle(Paint.Style.STROKE);
         progressPaint.setStrokeWidth(circleSize);
         progressPaint.setStrokeCap(Paint.Cap.ROUND);
@@ -85,7 +83,7 @@ public class CircleProgressView extends View {
         //绘制文字
         Rect rect = new Rect();
         String text;
-        DecimalFormat df = new DecimalFormat("0.00");
+        DecimalFormat df = new DecimalFormat("0.0");
         if (needPersent){
             text = df.format(progress)+"%";
         }else {
@@ -97,11 +95,73 @@ public class CircleProgressView extends View {
         canvas.drawText(text,(getWidth()-textWidth)/2,(getHeight()+textHeight)/2,textPaint);
     }
 
-    public void setProgress(float progress){
-//        this.progress = progress;
-//        invalidate();
-        startAni(progress);
+    /**
+     * 是否允许动画
+     * @param enableAni
+     */
+    public void setEnableAni(boolean enableAni){
+        this.needAni = enableAni;
     }
+
+    /**
+     * 设置进度
+     * @param progress
+     */
+    public void setProgress(float progress){
+        if (!needAni){
+            this.progress = progress;
+            invalidate();
+        }else {
+            startAni(progress);
+        }
+    }
+
+    /**
+     * 设置圆弧宽度
+     * @param circleWidth
+     */
+    public void setCircleWidth(int circleWidth){
+        this.circleSize = circleWidth;
+        invalidate();
+    }
+
+    /**
+     * 设置字体大小
+     * @param textSize
+     */
+    public void setTextSize(int textSize){
+        this.textSize = textSize;
+        invalidate();
+    }
+
+    /**
+     * 设置进度条颜色
+     * @param color
+     */
+    public void setProgressColor(int color){
+        this.progressColor = color;
+        invalidate();
+    }
+
+    /**
+     * 设置进度条背景色
+     * @param color
+     */
+    public void setProgressBgColor(int color){
+        this.bgCircleColor = color;
+        invalidate();
+    }
+
+    /**
+     * 设置字体颜色
+     * @param color
+     */
+    public void setTextColor(int color){
+        this.textColor = color;
+        invalidate();
+    }
+
+
 
     private void startAni(float persent){
         ValueAnimator animator = ValueAnimator.ofFloat(0,persent);
@@ -113,9 +173,8 @@ public class CircleProgressView extends View {
                 invalidate();
             }
         });
-        animator.setDuration(5000);
+        animator.setDuration(1000);
         animator.start();
-
     }
 
 }
